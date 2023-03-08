ff2674fba6c571153a6ac8e4f14a1c2a0cd1f8d3
Sakthi <sakthidharan00@gmail.com>
2023-03-08 18:23:40 +0530

    cors origin value changed

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-app.use(cors( {
-        origin:["http://localhost:3000","https://whatsapp-2n58.onrender.com"]
-}));
+app.use(cors());
 
 const dotEnv = require("dotenv")
 dotEnv.config();
